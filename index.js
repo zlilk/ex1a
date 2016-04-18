a25ef0c71571e7015c9955d27c491472f1f65c29
zlil kappa <zlilkp@gmail.com>
2016-04-18 17:03:30 +0300

    updated

--- index.js
+++ index.js
@@ -3,27 +3,25 @@ var bodyParser = require('body-parser');
 var gradesMd = require('./grades_md');
 var app = express();
 
-
 var port = process.env.PORT || 3000;
 
-//app.use('/assets', express.static(__dirname + '/public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-//app.all('*', function(req,res,next){
-    //console.log('logged in!');
-   // req.next();
-//});
 
+//geting all students data
 app.get('/studsData', function(req,res) {
    var studsData =  gradesMd.getAllStudentsData();
    res.json(studsData);
 });
 
+//getting a student grade by id
 app.get('/gradeById/:id', function(req,res) {
    var studGrade =  gradesMd.getStudGradeById(req.params.id);
    res.json(studGrade);
 });
 
+//getting all students grades by age
 app.get('/gradeByAge/:age', function(req,res) {
    var studsGrades =  gradesMd.getStudsGradeByAge(req.params.age);
    res.json(studsGrades);
